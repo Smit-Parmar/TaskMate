5388c43a85f6383a42df77edf7a05006c079e97f
Cyborg_7898 <www.smitraj333@gmail.com>
2020-07-24 08:15:46 +0530

    TaskMate Upadted

--- cyborg/settings.py
+++ cyborg/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,4 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 CRISPY_TEMPLATE_PACK="bootstrap4"
 LOGIN_REDIRECT_URL="todolist"
 LOGIN_URL="login"
+django_heroku.settings(locals())
