51bd5083c1e5f66c50c27b0716f0f16c29e1e648
Cyborg_7898 <www.smitraj333@gmail.com>
2020-07-24 08:27:58 +0530

    TaskMate Upadted

--- cyborg/settings.py
+++ cyborg/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles') #To push on heroku
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 CRISPY_TEMPLATE_PACK="bootstrap4"
